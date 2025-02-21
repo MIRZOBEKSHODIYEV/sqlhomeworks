d885883e49660daa548a6fddc1ee7b8f6630b7a9
MIRZOBEKSHODIYEV <mirzobekmirzobekshodiyev@gmail.com>
2025-02-21 19:36:15 +0500

    l

---
+++ lesson-4/homework/SQLQuery4.sql
@@ -0,0 +1,13 @@
+create table letters
+(letter char(1));
+
+insert into letters
+values ('a'), ('a'), ('a'), 
+  ('b'), ('c'), ('d'), ('e'), ('f');
+SELECT letter FROM letters 
+ORDER BY 
+    CASE 
+        WHEN letter = 'b' THEN 1 
+        ELSE 0 
+    END, 
+    letter;
